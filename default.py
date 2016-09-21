ecb5feed54cdaf09ee718d90741f0f7872086fdb
Charles Lesire <charles.lesire@onera.fr>
2016-09-21 14:57:16 +0200

    runway size for small demo

--- default.py
+++ default.py
@@ -8,16 +8,18 @@ from morse.builder import *
 
 # Runway
 runway = PassiveObject("./data/road.blend", "Plane")
-runway.scale = (10,5,1)
+## plane width is 2.5; length is 11
+#runway.scale = ((4000/11),(60/2.5),1)
+runway.scale = ((100./11),(20./2.5),1)
 
 # 4MOB
-'''robot = Morsy()
+robot = Morsy()
 keyboard = Keyboard()
 keyboard.properties(ControlType = 'Position')
 robot.append(keyboard)
 
 #robot.add_default_interface('ros')
-'''
+
 # set 'fastmode' to True to switch to wireframe mode
 env = Environment('', fastmode = False)
 env.set_camera_location([-18.0, -6.7, 10.8])
